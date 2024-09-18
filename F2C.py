fdcc16292ddb4f8f0d0f846f4653bc84fdea0763
ajmainwasif01 <nipunazmun@gmail.com>
2024-09-18 17:47:20 +0600

    F2C and C2F

---
+++ F2C.py
@@ -0,0 +1,3 @@
+Fahrenheit = int(input("Enter Value of Fahrenheit:"))
+Celsius = (5/9) * (Fahrenheit - 32)
+print("Result:" , Celsius)
---
+++ celsius-fahrenheit.py
@@ -0,0 +1,3 @@
+Celsius = int(input("Enter Value of Celsius:"))
+Fahrenheit =  (9 * Celsius / 5) + 32
+print("Result:" , Fahrenheit)
